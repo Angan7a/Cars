562f88b6c7c75f18c627fcc987bbbd6a85320168
Andrzej <vim>
2018-07-18 15:47:19 +0200

    Added throw - gears, capacity

--- PetrolEngine.cpp
+++ PetrolEngine.cpp
@@ -4,12 +4,20 @@
 #include <iostream>
 
 
-PetrolEngine::PetrolEngine(int power, float capacity, int gears) : capacity_(capacity), gears_(gears), currentGear_(0) {
+PetrolEngine::PetrolEngine(int power, float capacity, int gears) : currentGear_(0) {
 	if((power <=0) || (power>500)) {
 		throw InvalidRange("Power should be in the range [1-500]");
 	}
 	power_ = power;
-} 
+ 	if((gears <=0) || (gears > 5)) {
+		throw InvalidRange("Gears should be [1, 2, 3, 4, 5]");
+	}
+	gears_ = gears;
+ 	if((capacity <=500) || (capacity > 2000)) {
+		throw InvalidRange("Capacity should be in the range [500-2000]");
+	}
+	capacity_ = capacity;
+}
 
 void PetrolEngine::changeGear(int gear) {
 	std::cout << __FUNCTION__ << " to " << gear << std::endl; 
